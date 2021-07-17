d511ee37227c5445d0118f45ca5e2b1d4a74cd87
pelexsa <pelexsa@naver.com>
2021-07-17 18:49:24 +0900

    java study

---
+++ src/universityScenario/Course.java
@@ -0,0 +1,4 @@
+package universityScenario;
+
+public class Course {
+}
---
+++ src/universityScenario/Professor.java
@@ -0,0 +1,13 @@
+package universityScenario;
+
+public class Professor {
+    int professorUniqueNum;
+    String professorName;
+    String courseName;
+    String major;
+
+
+
+
+
+}
